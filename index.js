752326dafe57bf69601a828841bab255e913daec
lalaya21 <lalaya.shuckahosee@students.msswarriors.org>
2026-02-26 20:02:50 +0000

    done1

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-
+let counter = 100;
+let prefixIncrement = ++count;
+let postfixIncrement = count++;
+let prefixDecrement = --count;
+let postfixDecrement = count--;
+document.getElementById('result').innerHTML = 
